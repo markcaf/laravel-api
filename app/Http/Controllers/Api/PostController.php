99ea60657b68972bb6671ae39d0400c45b91a2c8
markcaf <marcocafassog@gmail.com>
2022-09-28 01:11:05 +0200

    refactor(Api\PostController): edit results on json

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -19,9 +19,7 @@ class PostController extends Controller
 
         return response()->json([
             'response' => true,
-            'results' => [
-                'data' => $posts
-            ]
+            'results' => $posts,
         ]);
     }
 
